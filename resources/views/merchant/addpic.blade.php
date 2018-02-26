d0eae2c6fb9a9fae6ff561d4899d897c71463711
chenlong <chenlong@jiayunhui.com>
2018-02-26 10:13:29 +0800

    ss

--- resources/views/merchant/addpic.blade.php
+++ resources/views/merchant/addpic.blade.php
@@ -68,7 +68,7 @@
         <div class="row cl">
             <label class="form-label col-xs-4 col-sm-2">图片摘要：</label>
             <div class="formControls col-xs-8 col-sm-9">
-                <textarea name="" cols="" rows="" class="textarea"  placeholder="说点什么...最少输入10个字符" datatype="*10-100" dragonfly="true" nullmsg="备注不能为空！" onKeyUp="$.Huitextarealength(this,200)"><?php echo isset($pic['explain'])?$pic['explain']:''; ?></textarea>
+                <textarea name="" cols="" rows="" class="textarea"  placeholder="说点什么...最少输入10个字符" datatype="*10-100" dragonfly="true" nullmsg="备注不能为空！" onKeyUp="$.Huitextarealength(this,200)"><?php echo $pic['explain']??''; ?></textarea>
                 <p class="textarea-numberbar"><em class="textarea-length">0</em>/200</p>
             </div>
         </div>
