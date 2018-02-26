caa0399061189214c128f8b0069615f54c4e68f8
chenlong <chenlong@jiayunhui.com>
2018-02-26 10:19:45 +0800

    sss

--- app/Http/Controllers/Merchant/LoginController.php
+++ app/Http/Controllers/Merchant/LoginController.php
@@ -51,7 +51,7 @@ class LoginController extends Controller
     public function logout()
     {
         session()->flush();
-        return redirect('/');
+        return redirect('/merchant/login');
     }
 
 }
