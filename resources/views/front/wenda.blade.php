dc48778d6b8e6395a76835c3fa4da9d3d3a00182
陈龙 <chenlong@jiayunhui.com>
2018-07-13 11:51:01 +0800

    优化

--- resources/views/front/wenda.blade.php
+++ resources/views/front/wenda.blade.php
@@ -2,7 +2,9 @@
 <!DOCTYPE html>
 <html lang="zh-cn">
 <head><meta charset="utf-8"><title><?php echo $title;?>-有榜网</title><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/><meta name="applicable-device" content="pc,mobile"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><meta http-equiv="Cache-Control" content="no-transform" /> <meta http-equiv="Cache-Control" content="no-siteapp" /><meta name="Keywords" content="<?php echo $city.$tenants['name'];?>,<?php echo $title;?>"><meta name="description" content=""><link rel="stylesheet" href="/css/style.css"><link rel="stylesheet" href="/css/wenda.css"><link rel="stylesheet" href="/css/glightbox.css"><script type="text/javascript" src="//m1.youbangkeyi.com/js/jquery-2.1.1.js"></script><script type="text/javascript" src="//m1.youbangkeyi.com/js/jquery.SuperSlide.2.1.1.js"></script><script type="text/javascript" src="/js/leftTime.min.js"></script><script src="//m1.youbangkeyi.com/js/TouchSlide.1.1.js"></script><script type="text/javascript" src="http://www.youbangkeyi.com/js/foot.js"></script></head>
-<body class="shop-index view"><div class="bgDiv"></div><div class="downNav"><div class="ask"><div class="tijiao"><p id="titlefield">提交信息，立即预约咨询商家</p><p class="current-li"><em>预约礼</em><?php echo isset($tenants['package'])?$tenants['package']:'是否有优惠？预约时请咨询商家';?></p></div><input type="hidden" name="tenantsId" id="tenantsId" value="<?php echo $tenants['id']??'';?>"><input type="hidden" name="source" id="source" value="1"><input type="tel" name="mobile" id="mobile" class="input" placeholder="请输入您的手机号 · 商家3分钟内会联系您！"><button type="submit" name="wapsubmit" id="tijiao" class="btn">免费预约咨询</button></div><div class="tishik" style="display:none;"><div class="tishizi"><p>预约成功</p></div></div></div>
+<body class="shop-index view">
+@include("front.tijiao")
+
 <div class="main"><header><div class="city_box" id="gr_zone_ids" data-id="110100"><?php echo $city;?></div><div class="logo"><a href="<?php echo '/'.$pycity.'/'.($tenants['shoptype']=='婚纱摄影'?'sheying':'hunli');?>"><?php echo $city.$tenants['shoptype'];?></a></div><a class="zxdh" href="tel:18500905723"></a></header>
     <div class="wendat">
         <h1><?php echo $ask['title'];?></h1>
@@ -218,81 +220,8 @@
     <script type="text/javascript" src="/js/city.js"></script>
     <script type="text/javascript" src="/js/sheyingcity.js"></script>
     <script type="text/javascript" src="/js/layer/layer.js"></script>
-    <script type="text/javascript">
-    $('button[name=wapsubmit]').click(function(){
-        var tenantsId = $('#tenantsId').val();
-        var phone = $('#mobile').val();
-        var city = $('#pcity').val();
-        var source = $("#source").val();
 
-        if(!phone){
-            alert('手机必填');
-            return false;
-        }
-        if(phone){
-            if(!(/^1[34578]\d{9}$/.test(phone))){
-                alert("亲，手机号码填写的不对哦");
-                return false;
-            }
-        }
-        $(this).attr('disabled', true);
-        $.ajax({
-            url: "/saveview",
-            type: "post",
-            dataType: "json",
-            data: {'tenantsId': tenantsId,"city":city,'phone': phone,'source':source},
-            success: function(data){
-                console.log(data);
-                if(data.result=='00'){
-                    $(".ask").css("display","none");
-                    $(".tishik").css("display","block");
-        }
-            }
-        });
-    });
-    $(function(){
-    $(".anliclick").click(function(){
-        $("#source").val(3);
-        $("#titlefield").html("提交信息，快速获取报价");
-        $("#tijiao").html("免费获取报价");
-        $("#mobile").attr("placeholder","请输入手机号 · 商家3分钟内会联系您！");
-    })
-    $(".shenqingclick").click(function(){
-        $("#source").val(1);
-        $("#titlefield").html("提交信息，申请大数据深度推荐");
-        $("#tijiao").html("免费申请");
-        $("#mobile").attr("placeholder","请输入手机号，分析师根据需求为您推荐！");
-    })
-    $(".liwuclick").click(function(){
-        $("#source").val(2);
-        $("#titlefield").html("提交信息，领取优惠并预约");
-        $("#tijiao").html("免费领取优惠");
-        $("#mobile").attr("placeholder","请输入手机号 · 商家3分钟内会联系您！");
-    })
-
-    $(".dangqiclick").click(function(){
-        $("#source").val(4);
-        $("#titlefield").html("提交信息，预约拍摄档期");
-        $("#tijiao").html("免费咨询档期");
-        $("#mobile").attr("placeholder","请输入手机号 · 商家3分钟内会联系您！");
-    })
-        $(".freeclick").click(function(){
-            $("#source").val(5);
-            $("#titlefield").html("提交信息，预约拍摄档期");
-            $("#tijiao").html("免费咨询档期");
-            $("#mobile").attr("placeholder","请输入手机号 · 商家3分钟内会联系您！");
-        })
-    });
-</script>
-<?php if(isset($tenants['pcount']['count']) &&$tenants['pcount']['count']>0 ) { ?>
-<div id="zxsl"></div>
-<?php } ?>
-<div class="zxyy"><em class="down yuyueclick">免费预约咨询 · 3分钟响应</em></div>
-    <?php if(isset($tenants['pcount']['count']) &&$tenants['pcount']['count']>0 ) { ?>
-    <script type="text/javascript">
-    $('#zxsl').html('今天已有<?php echo $tenants['pcount']['count'];?>人预约该商家').addClass('alert-success').show().delay(2000).fadeOut();
-    </script>
-    <?php } ?>
+    @include("front.tijiaojs")
 </div>
 </body>
 </html>
